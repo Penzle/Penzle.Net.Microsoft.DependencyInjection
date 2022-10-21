a77d954c2b7f46e259527c3dbed06726b55539a6
Admir <admir.mujkic@penzle.com>
2022-10-21 21:00:23 +0200

    Restore previously deleted files
    Restore files that were erased in the past.

---
+++ src/PenzleServiceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Penzle.Net.Microsoft.DependencyInjection;
+
+public sealed class PenzleServiceConfiguration
+{
+    public Uri BaseUri { get; set; }
+    public string ApiDeliveryKey { get; set; }
+    public string ApiManagementKey { get; set; }
+    public TimeSpan TimeOut { get; set; }
+    public string Project { get; set; }
+    public string Environment { get; set; }
+}
